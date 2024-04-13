f1ea3baacb22a2e665909240846a0764d04dd4f7
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-04-13 02:33:44 -0400

    finalized dashboard styling

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -29,42 +29,51 @@ const Dashboard = ({ handleLogout }) => {
   }, []);
 
   return (
-    <div className="bg-slate-900 pb-24 pt-10">
+    <div
+      className="bg-slate-900 pb-24 pt-10"
+      style={{
+        backgroundImage: `url(${`https://res.cloudinary.com/dvmczcg3f/image/upload/c_crop,ar_16:9/v1712988101/pattern_ptsyaw.png`})`,
+        backgroundSize: "cover",
+      }}
+    >
       {/* <h2 className="bg-red-400 py-8 text-center text-2xl">
         Dashboard Component
       </h2> */}
-      <div className="">
-        <div className="text-center">
-          {/* {user && (
+      <div className="text-center mt-16">
+        <div className="flex justify-center">
+          <div className="bg-slate-900/70 shadow-2xl mx-96 rounded-2xl p-10 border-2 border-teal-300/50">
+            {/* {user && (
           <h1 className="text-2xl my-8">
             Welcome, {user.username[0].toUpperCase()}
             {user.username.slice(1).toLowerCase()}
           </h1>
         )} */}
-          <div className="flex justify-center">
-            <img
-              src={user.profile_picture}
-              alt="user image"
-              className="w-48 rounded-full border-4 border-slate-300 mt-10 "
-            />
-          </div>
-          <h1 className="text-2xl font-bold my-5 text-slate-200">Username:</h1>
-          <h5 className="font-semibold bg-slate-100 rounded inline-block p-2 shadow">
-            {user.username[0].toUpperCase()}
-            {user.username.slice(1).toLowerCase()}
-          </h5>
-          <h6 className="text-2xl font-bold my-5 text-slate-200">
-            Joined Tale Blazers on:
-          </h6>
-          <h2 className=" font-semibold bg-slate-100 rounded inline-block p-2 shadow">
-            {formatDate(user.created_at)}
-          </h2>
-          <h1 className="text-2xl font-bold mt-5 text-slate-200">Bio:</h1>
-          <h2 className="font-semibold bg-slate-100 rounded inline-block p-2 shadow w-96 mt-5">
-            {user.bio}
-          </h2>
+            <div className="flex justify-center">
+              <img
+                src={user.profile_picture}
+                alt="user image"
+                className="w-48 rounded-full border-4 border-slate-300 "
+              />
+            </div>
+            <h1 className="text-2xl font-bold my-5 text-slate-200">
+              Username:
+            </h1>
+            <h5 className="font-semibold bg-slate-100 rounded inline-block p-2 shadow text-slate-900">
+              {user.username[0].toUpperCase()}
+              {user.username.slice(1).toLowerCase()}
+            </h5>
+            <h6 className="text-2xl font-bold my-5 text-slate-200">
+              Joined Tale Blazers on:
+            </h6>
+            <h2 className=" font-semibold bg-slate-100 rounded inline-block p-2 shadow text-slate-900">
+              {formatDate(user.created_at)}
+            </h2>
+            <h1 className="text-2xl font-bold mt-5 text-slate-200">Bio:</h1>
+            <h2 className="font-semibold bg-slate-100 rounded inline-block p-2 shadow w-96 mt-5 text-slate-900">
+              {user.bio}
+            </h2>
 
-          {/* {userData.map(
+            {/* {userData.map(
         ({ id, username, email, profile_picture, bio, created_at }) => (
           <div className="" key={id}>
             <div>username</div>
@@ -72,7 +81,8 @@ const Dashboard = ({ handleLogout }) => {
         )
       )} */}
 
-          {/* Use user data as needed, for example: */}
+            {/* Use user data as needed, for example: */}
+          </div>
         </div>
         <div className="text-center mt-5">
           <button
