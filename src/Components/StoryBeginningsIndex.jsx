c6db32ee8c19ad878f7c02a82ad9ca8493e93d7a
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-04-17 15:41:39 -0400

    rounded landing page cards

--- src/Components/StoryBeginningsIndex.jsx
+++ src/Components/StoryBeginningsIndex.jsx
@@ -10,15 +10,15 @@ const StoryBeginnings = ({ storyBeginnings }) => {
             <div key={id}>
               <Link to={`/storydetails/${id}`}>
                 <div className="shadow-2xl mb-10 w-5/6 mx-auto md:w-1/3 hover:scale-110 transition-transform duration-300">
-                  <div className="flex justify-between bg-slate-800 shadow">
+                  <div className="flex justify-between bg-slate-800 shadow rounded-t-xl">
                     <div className="text-xl font-semibold text-slate-50 p-2 pl-3 pt-3">
                       {title}
                     </div>
-                    <div className=" inline-block bg-slate-100 px-2 mb-2 mt-2 mr-3 py-1 italic">
+                    <div className=" inline-block bg-slate-100 px-2 mb-2 mt-2 mr-3 py-1 italic rounded-lg">
                       {genre}
                     </div>
                   </div>
-                  <div className="text-center shadow p-3 bg-slate-700 text-slate-100">
+                  <div className="text-center shadow p-3 bg-slate-700 text-slate-100 rounded-b-xl">
                     <span className="line-clamp-1">{description}</span>
                   </div>
                 </div>
