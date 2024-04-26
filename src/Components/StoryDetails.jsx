48ab20ca4bfa0aeeb65c9ed15723180b6702a956
Alex_de_code <alexandergonzalez@pursuit.orgclear>
2024-04-25 22:33:21 -0400

    style test w/ padding/margin

--- src/Components/StoryDetails.jsx
+++ src/Components/StoryDetails.jsx
@@ -248,7 +248,7 @@ const StoryDetails = () => {
       <div className="flex justify-center mt-5 ">
         <div className="bg-slate-600 w-96 lg:w-192 shadow-xl rounded-b-3xl">
           <div className="flex flex-row">
-            <h2 className="text-2xl bg-slate-600 py-2 text-slate-200 font-semibold p-3 shadow">
+            <h2 className="text-2xl bg-slate-600 py-2 text-slate-200 font-semibold p-6 shadow">
               {allStoryEndingsForSingleStory[currentIndex]?.title}
             </h2>
             <div className="ml-auto mr-2 flex items-center">
@@ -260,7 +260,7 @@ const StoryDetails = () => {
                 }
                 className="bg-teal-400 hover:bg-slate-300 font-semibold p-1 m-1 rounded-full inline-flex items-center ml-auto mr-3"
               >
-                <PencilLine size={26} />
+                <PencilLine size={36} />
               </button>
               <button
                 onClick={() =>
@@ -273,7 +273,7 @@ const StoryDetails = () => {
             </div>
           </div>
           <hr className="border-2 border-slate-700" />
-          <p className="pb-4 pt-3 pl-4 pr-4 text-slate-200">
+          <p className="pb-8 pt-6 pl-8 pr-8 text-slate-200">
             <div className="flex flex-row py-1 justify-between">
               <button
                 onClick={goToPreviousEnding}
@@ -296,7 +296,7 @@ const StoryDetails = () => {
           </p>
         </div>
       </div>
-      <div className="text-white text-center ">
+      <div className="text-white text-center mt-2">
         (Entry {currentIndex + 1} of {allStoryEndingsForSingleStory.length})
       </div>
     </div>
